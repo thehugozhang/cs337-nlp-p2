c4a9840a86e1ea5f74316b8bdb6fbeb617948088
Hugo Zhang <hugozhang2023@u.northwestern.edu>
2023-03-04 13:40:50 -0600

    start rasa custom actions for recipe parsing, navigation utterances

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,77 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# This is a simple example for a custom action which utters "Hello World!"
+###############################################################
+# Recipe parsing / slot memory utterance.
+###############################################################
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionParseRecipe(Action):
+
+    def name(self) -> Text:
+        return "action_parse_recipe"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Great! Please provide a recipe URL to get started.")
+
+        return []
+
+###############################################################
+# Navigation utterances.
+###############################################################
+
+class ActionPreviousStep(Action):
+
+    def name(self) -> Text:
+        return "action_previous_step"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Implement previous step here.")
+
+        return []
+
+class ActionNextStep(Action):
+
+    def name(self) -> Text:
+        return "action_next_step"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Implement next step here.")
+
+        return []
+
+class ActionNthStep(Action):
+
+    def name(self) -> Text:
+        return "action_nth_step"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Implement nth step here.")
+
+        return []
+
+class ActionRepeatStep(Action):
+
+    def name(self) -> Text:
+        return "action_repeat_step"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Implement repeat step here.")
+
+        return []
